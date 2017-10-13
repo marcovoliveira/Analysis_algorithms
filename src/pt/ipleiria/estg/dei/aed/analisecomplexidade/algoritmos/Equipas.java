cc46379fc88b882a9fecf8cbebde96e7f28e4aff
rezken1 <marcooliveirapias@gmail.com>
2017-10-13 17:41:55 +0100

    Estatistica

--- src/pt/ipleiria/estg/dei/aed/analisecomplexidade/algoritmos/Equipas.java
+++ src/pt/ipleiria/estg/dei/aed/analisecomplexidade/algoritmos/Equipas.java
@@ -1,31 +1,52 @@
 package pt.ipleiria.estg.dei.aed.analisecomplexidade.algoritmos;
 
+import pt.ipleiria.estg.dei.aed.utils.EstatisticaChamadas;
+import pt.ipleiria.estg.dei.aed.utils.EstatisticaDeComparacoes;
+
 import java.awt.geom.Point2D;
+import java.util.Arrays;
 
 public class Equipas {
 
-    public void executar (Point2D... equipas){
-        int min = 0;
-        int out = 1;
-        double distance = 999999999;
-        for (int i = 0; i < equipas.length; i++ )
-        {
+    public void executar(EstatisticaDeComparacoes estatistica, Point2D... equipas) {
+        int Ponto1 = 0;
+        int Ponto2 = 1;
+        double distance = Double.POSITIVE_INFINITY;
 
-          for (int j = i+1; j < equipas.length; j++)
-          {
-                      System.out.println(equipas[i] + " vs " + equipas[j] +
-                      "Distancia:" + equipas[i].distance(equipas[j]) );
+        for (int i = 0; i < equipas.length; i++) {
+            estatistica.incrementarComparacoes();
+            for (int j = i + 1; j < equipas.length; j++) {
+                estatistica.incrementarComparacoes();
+                System.out.println(equipas[i] + " vs " + equipas[j] +
+                        "Distancia:" + equipas[i].distance(equipas[j]));
 
-                  if (equipas[i].distance(equipas[j]) < distance)
-                  {
-                        min = i;
-                        out = j;
-                        distance = equipas[min].distance(equipas[out]);
-                  }
+                if (equipas[i].distance(equipas[j]) < distance) {
+                    Ponto1 = i;
+                    Ponto2 = j;
+                    distance = equipas[Ponto1].distance(equipas[Ponto2]);
                 }
-             }
-                System.out.println("Resultado: \n" + equipas[min] + " com " + equipas[out] + " Distancia: " + distance);
             }
         }
+        System.out.println("Resultado: \n" + equipas[Ponto1] + " com " + equipas[Ponto2] + " Distancia: " + distance);
+    }
+
+    public EstatisticaDeComparacoes getEstatistica(Point2D... valores) {
+        try {
+            EstatisticaDeComparacoes estatistica = new EstatisticaDeComparacoes(valores.length);
+
+            executar(estatistica, valores);
+            estatistica.parar();
+
+            estatistica.apresentar();
+
+            return estatistica;
+        }
+        catch (IllegalArgumentException ex) {
+            System.out.println("Não foi possivel calcular o maximo de " + Arrays.toString(valores));
+            return null;
+        }
+    }
+}
+
 
 
--- src/pt/ipleiria/estg/dei/aed/analisecomplexidade/utilizacao/MainPraticaEquipas.java
+++ src/pt/ipleiria/estg/dei/aed/analisecomplexidade/utilizacao/MainPraticaEquipas.java
@@ -1,14 +1,36 @@
 package pt.ipleiria.estg.dei.aed.analisecomplexidade.utilizacao;
 
 import pt.ipleiria.estg.dei.aed.analisecomplexidade.algoritmos.Equipas;
+import pt.ipleiria.estg.dei.aed.utils.Estatistica;
+import pt.ipleiria.estg.dei.aed.utils.VetorDeInteiros;
+import pt.ipleiria.estg.dei.aed.utils.VetorDePoint2D;
+import pt.ipleiria.estg.dei.aed.utils.visualizacao.VisualizadorEstatisticas;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainPraticaEquipas {
     public  MainPraticaEquipas() {
+
         Equipas equipas = new Equipas();
-        equipas.executar(new Point2D.Double(0,0), new Point2D.Double(5,5),
-                new Point2D.Double(10,15), new Point2D.Double(8,9));
+        int j = 4;
+        do {
+            equipas.getEstatistica(VetorDePoint2D.criarAleatorio(50));
+            j++;
+        }
+        while (j < 10);
+
+        List<Estatistica> estatisticas = new ArrayList<>();
+
+        for (int i = 10; i <= 100; i+=10){
+            estatisticas.add(equipas.getEstatistica(VetorDePoint2D.criarAleatorio(5)));
+        }
+
+
+        VisualizadorEstatisticas visualizador = new VisualizadorEstatisticas();
+        visualizador.adicionarEstatisticas("Maximo", estatisticas);
+        visualizador.visualizar();
     }
 
     public static void main (String[] args) {
